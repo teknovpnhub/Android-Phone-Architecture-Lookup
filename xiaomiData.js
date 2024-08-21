872eb206ea2b5ac91a97c78bc8be3d1dcb3097a8
teknovpnhub <167628741+teknovpnhub@users.noreply.github.com>
2024-08-21 20:04:41 +0330

    Add files via upload

---
+++ xiaomiData.js
@@ -0,0 +1,131 @@
+const xiaomiData = [
+  { "name": "Xiaomi Mi 1", "architecture": "ARM Cortex-A8 (32-bit)" },
+  { "name": "Xiaomi Mi 2", "architecture": "ARM Cortex-A15 (32-bit)" },
+  { "name": "Xiaomi Mi 3", "architecture": "ARM Cortex-A15 (32-bit)" },
+  { "name": "Xiaomi Mi 4", "architecture": "ARM Cortex-A15 (32-bit)" },
+  { "name": "Xiaomi Mi 5", "architecture": "ARM Cortex-A72 (64-bit)" },
+  { "name": "Xiaomi Mi 6", "architecture": "ARM Cortex-A73 (64-bit)" },
+  { "name": "Xiaomi Mi 8", "architecture": "ARM Cortex-A75 (64-bit)" },
+  { "name": "Xiaomi Mi 9", "architecture": "ARM Cortex-A76 (64-bit)" },
+  { "name": "Xiaomi Mi 10", "architecture": "ARM Cortex-A77 (64-bit)" },
+  { "name": "Xiaomi Mi 11", "architecture": "ARM Cortex-X1 (64-bit)" },
+  { "name": "Xiaomi Mi Note 10", "architecture": "ARM Cortex-A76 & ARM Cortex-A55 (64-bit)" },
+  { "name": "Xiaomi Mi Note 10 Pro", "architecture": "ARM Cortex-A76 & ARM Cortex-A55 (64-bit)" },
+  { "name": "Xiaomi Mi Note 10 Lite", "architecture": "ARM Cortex-A76 & ARM Cortex-A55 (64-bit)" },
+  {
+  "name": "POCO M6",
+  "architecture": "Qualcomm Snapdragon 480 (SM4350-AC) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO F6 Pro",
+  "architecture": "Qualcomm Snapdragon 845 (SM8150-AC) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO F6",
+  "architecture": "Qualcomm Snapdragon 660 (SM6125) with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO Pad",
+  "architecture": "MediaTek Helio G99 with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO M6 Pro",
+  "architecture": "Qualcomm Snapdragon 778G (SM7325-AE) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO X6 Pro",
+  "architecture": "Qualcomm Snapdragon 732G (SM7125) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO X6",
+  "architecture": "Qualcomm Snapdragon 665 (SM6125) with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO C65",
+  "architecture": "Qualcomm Snapdragon 460 (SM4250) with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO F5 Pro",
+  "architecture": "Qualcomm Snapdragon 860 (SM8150-AC) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO F5",
+  "architecture": "Qualcomm Snapdragon 660 (SM6125) with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO X5 Pro 5G",
+  "architecture": "MediaTek Dimensity 900 (MT6893Z) with big.LITTLE architecture (ARM Cortex-A78 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO X5 5G",
+  "architecture": "MediaTek Dimensity 920 (MT6877) with big.LITTLE architecture (ARM Cortex-A78 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M5s",
+  "architecture": "MediaTek Helio G95 (MT6785) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M5",
+  "architecture": "MediaTek Helio G99 (MT6781) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M4 5G",
+  "architecture": "MediaTek Dimensity 700 (MT6833) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO F4",
+  "architecture": "Qualcomm Snapdragon 870 (SM8475) with big.LITTLE architecture (ARM Cortex-X1 & ARM Cortex-A710 & ARM Cortex-A510) (64-bit)"
+},
+{
+  "name": "POCO X4 GT",
+  "architecture": "Qualcomm Snapdragon 695 (SM6375) with big.LITTLE architecture (ARM Cortex-A78 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO C40",
+  "architecture": "Qualcomm Snapdragon 680 (SM6115) with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO F4 GT",
+  "architecture": "Qualcomm Snapdragon 888 (SM8350) with big.LITTLE architecture (ARM Cortex-X1 & ARM Cortex-A78 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M4 Pro",
+  "architecture": "MediaTek Dimensity 810 (MT6833) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO X4 Pro 5G",
+  "architecture": "Qualcomm Snapdragon 695 (SM6375) with big.LITTLE architecture (ARM Cortex-A78 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M4 Pro 5G",
+  "architecture": "MediaTek Dimensity 700 (MT6833) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO X3 GT",
+  "architecture": "MediaTek Dimensity 1100 (MT6891) with big.LITTLE architecture (ARM Cortex-A78 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M3 Pro 5G",
+  "architecture": "MediaTek Dimensity 720 (MT6853) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO F3",
+  "architecture": "Qualcomm Snapdragon 870 (SM8350) with big.LITTLE architecture (ARM Cortex-X1 & ARM Cortex-A710 & ARM Cortex-A510) (64-bit)"
+},
+{
+  "name": "POCO X3 Pro",
+  "architecture": "Qualcomm Snapdragon 860 (SM8150-AC) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO X3 NFC",
+  "architecture": "Qualcomm Snapdragon 732G (SM7125) with big.LITTLE architecture (ARM Cortex-A76 & ARM Cortex-A55) (64-bit)"
+},
+{
+  "name": "POCO M3",
+"architecture": "Qualcomm Snapdragon 662 (SM6115) with big.LITTLE architecture (ARM Cortex-A73 & ARM Cortex-A53) (64-bit)"
+},
+{
+  "name": "POCO F2 Pro",
+  "architecture": "Qualcomm Snapdragon 865 (SM8250) with big.LITTLE architecture (ARM Cortex-A77 & ARM Cortex-A55) (64-bit)"
+}
+];
